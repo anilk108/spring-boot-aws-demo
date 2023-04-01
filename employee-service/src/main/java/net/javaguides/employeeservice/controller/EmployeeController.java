8e696feaf513654f4d315cc08ac25fd295db1e3f
anilk108 <anil152169@gmail.com>
2023-04-01 03:30:22 -0500

    Swagger API Documentation (Employee Service) -> (Customized) These annotations used to provide the information about the each rest api at swagger ui.

--- employee-service/src/main/java/net/javaguides/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/controller/EmployeeController.java
@@ -1,5 +1,8 @@
 package net.javaguides.employeeservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.javaguides.employeeservice.dto.APIResponseDto;
@@ -12,9 +15,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/employees")
 @AllArgsConstructor
+@Tag(
+        name = "CRUD REST API's for Employee Service",
+        description = "Create Employee and Get Employee - API's"
+)
 public class EmployeeController {
     private EmployeeService employeeService;
 
+    @Operation(
+            summary = "Create Employee Rest API",
+            description = "any description you want!"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 Created"
+    )
     // @Valid used for the api validation using Hibernate, key attributes added in dto files
     @PostMapping
     public ResponseEntity<EmployeeDto> saveEmployee(@Valid @RequestBody EmployeeDto employeeDto) {
